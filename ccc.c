9d6304fb1268faebe0a4b992b03db32f6099c094
tsyin_mobi <1115653102@qq.com>
2022-03-07 16:56:18 +0800

    数组PPT补充

--- ccc.c
+++ ccc.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 int main(void){
-    int arr[8]={1,2,3,4,5,6,7,8};
-    int *ptr;
-    ptr = arr;
-    printf("%d,%d",arr[1],*(ptr +1));
+    int arr[2][4]={1,2,3,4,5,6,7,8};
+    int (*p)[4] = arr;
+    printf("p = %p",p);
+    printf("p + 1 = %d",**(p + 1));
     return 0;
+
 }
