018d6785526d4738b2349e9ac3e2fee06c74f56b
tsyin_mobi <1115653102@qq.com>
2022-03-07 11:00:46 +0800

    3-7

--- ccc.c
+++ ccc.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 
 int main(void){
-    int arr[2][3] = {{1,2},{3,4},{5,6}};
-    for(int i = 0;i<2;i++)
-        for(int j = 0;j<3;j++)
-        {
-            printf("%d",arr[i][j]);
-        }
+    int arr[8]={1,2,3,4,5,6,7,8};
+    int *ptr;
+    ptr = arr;
+    printf("%d,%d",arr[1],*(ptr +1));
     return 0;
 }
